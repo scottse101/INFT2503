7ba4c914b8e7b5c57bad6c7ffe8e897d7226f028
Scott Sanya Emonanekkul <scottse@stud.ntnu.no>
2024-10-07 23:32:18 +0200

    Added exercise 1 of the solution 7 to the project

---
+++ solution7/main.cpp
@@ -0,0 +1,51 @@
+#include "fraction.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void print(const string &text, const Fraction &broek) {
+  cout << text << broek.numerator << " / " << broek.denominator << endl;
+}
+
+int main() {
+  Fraction a(10, 20);
+  Fraction b(3, 4);
+  Fraction c;
+  c.set(5);
+  Fraction d = a / b;
+
+  print("a = ", a);
+  print("b = ", b);
+  print("c = ", c);
+  print("d = ", d);
+
+  b += a;
+  ++c;
+  d *= d;
+
+  print("b = ", b);
+  print("c = ", c);
+  print("d = ", d);
+
+  c = a + b - d * a;
+  c = -c;
+
+  print("c = ", c);
+
+  if (a + b != c + d)
+    cout << "a + b != c + d" << endl;
+  else
+    cout << " a + b == c + d" << endl;
+  while (b > a)
+    b -= a;
+  print("b = ", b);
+
+  Fraction e(3, 5);
+  Fraction result1 = e - 2;  
+  Fraction result2 = 2 - e;  
+
+  print("e - 2 = ", result1);
+  print("2 - e = ", result2);
+}
+
